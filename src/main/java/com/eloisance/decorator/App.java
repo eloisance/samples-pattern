1106e19e8f674db2f891e25e68f60ec561e5595a
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-10-24 13:53:02 +0200

    First commit

---
+++ src/main/java/com/eloisance/decorator/App.java
@@ -0,0 +1,9 @@
+package main.java.com.eloisance.decorator;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/eloisance/proxy/App.java
@@ -0,0 +1,9 @@
+package main.java.com.eloisance.proxy;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
